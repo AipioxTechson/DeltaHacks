a555f8a3fdbc77c73c0fe07152347856950cadea
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-03-06 14:58:00 -0500

    add database configs and sample routes

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,59 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
 
+var serviceAccount = require('./admin.json');
+
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount),
+databaseURL: "https://deltahacks-d105e-default-rtdb.firebaseio.com/",
+authDomain: "deltahacks-d105e.firebaseapp.com",
+});
+
+// CONSTANTS
 const cors = require('cors');
 const express = require('express');
-const PORT = 3000;
 
 const app = express();
 
 app.use(cors());
 
+var db=admin.database();
+
+var userRef=db.ref("users");
+
+//Helper functions
+const getUsers = (res) => {
+  return userRef.once('value',(snap) => {
+    res.status(200).json({"users":snap.val()})
+    });
+};
+
+const createUser = (res) => {
+  userRef.push({
+    id:22,
+    name:"Jane Doe",
+    email:"jane@doe.com",
+    website:"https://jane.foo.bar"
+  }, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(300).json({"msg":"Something went wrong","error":err});
+    } else {
+      res.status(200).json({"msg":"user created sucessfully"});
+    }
+  })
+}
+
+
+
+//END POINTS
 app.get('/hello', (req,res) => {
-  res.send("hello there");
+  console.log("Hello!");
+  getUsers(res);
 })
 
-app.listen(PORT, () => {
-  console.log('Server is listening on PORT', PORT);
+app.post('/createUser', (req,res) => {
+  createUser(res);
 });
 
 // // Create and Deploy Your First Cloud Functions
